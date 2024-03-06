eaa0e5747bea0e30069a79e60b9f69711af08687
enzo0 <enzo.claverie@limayrac.fr>
2024-03-06 11:46:32 +0100

    visuelAppli

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // Rediriger vers la page de connexion
+    window.location.href = '/login';
+});
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+    // Rediriger vers la page de création de compte
+    window.location.href = '/signup';
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./routes/api');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Route principale
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// Routes pour les pages de connexion et de création de compte
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'signup.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
